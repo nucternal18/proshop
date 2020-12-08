2dd34471c6e139efa663750ab03428e99a01afc8
A Oladipupo-Usoro <aolausoro@gmail.com>
2020-12-08 23:49:59 +0000

    setup homescreen, productscreen and card component. setup express backend api

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,21 @@
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import { Container } from 'react-bootstrap';
 import Header from './component/Header';
 import Footer from './component/Footer';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 function App() {
   return (
-    <>
+    <Router>
       <Header />
       <main className="py-3">
         <Container>
-          <h1>Blooms Hair Shop</h1>
+          <Route exact path="/" component={HomeScreen} />
+          <Route path="/product/:id" component={ProductScreen} />
         </Container>
       </main>
       <Footer />
-    </>
+    </Router>
   );
 }
 
