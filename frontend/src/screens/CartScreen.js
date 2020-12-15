4d968996fb11567385a4974dd815d2c0faa7fe74
A Oladipupo-Usoro <aolausoro@gmail.com>
2020-12-15 23:59:24 +0000

    added state management using redux, cart reducer and add to cart functionality

---
+++ frontend/src/screens/CartScreen.js
@@ -0,0 +1,49 @@
+import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  Row,
+  Col,
+  ListGroup,
+  Image,
+  Form,
+  Button,
+  Card,
+} from 'react-bootstrap';
+import Message from '../component/Message';
+import { addToCart } from '../actions/cardActions';
+
+const CartScreen = ({ match, location, history }) => {
+  const productId = match.params.id;
+    console.log(productId)
+  const qty = location.search ? Number(location.search.split('=')[1]) : 1;
+    console.log(qty)
+  const dispatch = useDispatch();
+
+  const cart = useDispatch((state) => state.cart);
+  const { cartItems } = cart;
+  console.log(cartItems);
+
+  useEffect(() => {
+    if (productId) {
+      dispatch(addToCart(productId, qty));
+    }
+  }, [dispatch, productId, qty]);
+  return (
+    <Row>
+      <Col md={8}>
+        {cartItems.length === 0 ? (
+          <Message>
+            Your cart is empty <Link to='/'>Go Back</Link>
+          </Message>
+        ) : (
+          <ListGroup variant='flush'></ListGroup>
+        )}
+      </Col>
+      <Col md={2}></Col>
+      <Col md={2}></Col>
+    </Row>
+  );
+};
+
+export default CartScreen;
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,34 +1,39 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios'
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Row, Col } from 'react-bootstrap';
 import Product from '../component/Product';
-
-
+import Message from '../component/Message';
+import Loader from '../component/Loader';
+import { listProducts } from '../actions/productActions';
 
 const HomeScreen = () => {
-  const [products, setProducts] = useState([])
+  const dispatch = useDispatch();
+
+  const productsList = useSelector((state) => state.productsList);
+  const { loading, error, products } = productsList;
 
   useEffect(() => {
-    const fetchProducts = async () => {
-      const {data} = await axios.get('/api/products');
-      
-      setProducts(data);
-    }
-    
-    fetchProducts()
-  },[])
+    dispatch(listProducts());
+  }, [dispatch]);
+
   return (
     <>
       <h1>Latest Products</h1>
-      <Row>
-        {products.map((product) => {
-          return (
-            <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-              <Product product={product} />
-            </Col>
-          );
-        })}
-      </Row>
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant="danger">{error}</Message>
+      ) : (
+        <Row>
+          {products.map((product) => {
+            return (
+              <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                <Product product={product} />
+              </Col>
+            );
+          })}
+        </Row>
+      )}
     </>
   );
 };
