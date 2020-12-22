bb1d70478cb2f076d4c70ea006fe4eb834a2b672
A Oladipupo-Usoro <aolausoro@gmail.com>
2020-12-22 23:03:56 +0000

    setup login screen, register screen and user authentication in the backend and frontend

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,15 @@
 import express from 'express';
 const router = express.Router();
-import { authUser, getUserProfile, registerUser } from '../controllers/userController.js';
-import {protect} from  '../middleware/authMiddleware.js'
+import {
+  authUser,
+  getUserProfile,
+  registerUser,
+  updateUserProfile,
+} from '../controllers/userController.js';
+import { protect } from '../middleware/authMiddleware.js';
 
 router.post('/login', authUser);
-router.route('/profile').get(protect, getUserProfile)
-router.route('/').post(registerUser)
+router.route('/profile').get(protect, getUserProfile).put(protect, updateUserProfile);
+router.route('/').post(registerUser);
 
 export default router;
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,25 @@
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
 import Header from './component/Header';
 import Footer from './component/Footer';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 import CartScreen from './screens/CartScreen';
+import LoginScreen from './screens/LoginScreen';
+import RegisterScreen from './screens/RegisterScreen';
+import ProfileScreen from './screens/ProfileScreen';
 function App() {
   return (
     <Router>
       <Header />
-      <main className="py-3">
+      <main className='py-3'>
         <Container>
-          <Route exact path="/" component={HomeScreen} />
-          <Route path="/product/:id" component={ProductScreen} />
-          <Route path="/cart/:id?" component={CartScreen} />
+          <Route exact path='/login' component={LoginScreen} />
+          <Route exact path='/register' component={RegisterScreen} />
+          <Route exact path='/profile' component={ProfileScreen} />
+          <Route path='/product/:id' component={ProductScreen} />
+          <Route path='/cart/:id?' component={CartScreen} />
+          <Route exact path='/' component={HomeScreen} />
         </Container>
       </main>
       <Footer />
